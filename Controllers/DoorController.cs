3348617de4011a29b0051c0a7832b47f7edf2155
Tsvyatkova,Tsvetelina T <t.tsvyatkova@student.fontys.nl>
2021-07-01 01:00:52 +0200

    clay test

---
+++ Controllers/DoorController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using ClayTest.Entities;
+using ClayTest.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ClayTest.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class DoorController : ControllerBase
+    {
+        private readonly ILogger<DoorController> _logger;
+
+        public DoorController(ILogger<DoorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Door>> GetAll() => DoorService.GetAll();
+
+        [Authorize(Roles = Role.Admin)]
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Door door)
+        {
+            if (id != door.Id)
+                return BadRequest();
+
+            Door existingDoor = DoorService.Get(id);
+            if (existingDoor is null)
+                return NotFound();
+
+            DoorService.Update(door);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Entities/Door.cs
@@ -0,0 +1,10 @@
+﻿namespace ClayTest.Entities
+{
+    public class Door
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsLocked { get; set; }
+
+    }
+}
